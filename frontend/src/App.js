086ec0f3aaee7b680394f8246823688d42347f1a
AimanRusyaidi <aimanadam0525@gmail.com>
2024-08-28 16:09:22 +0800

    sidebar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
 
-function App() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch("http://localhost:5000/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-      .catch((err) => console.log(err));
-  });
-  return <div>{data ? <h1>{data}</h1> : <h1>Loading...</h1>}</div>;
-}
+
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainContent from "./MainContent";
+import './Styles/Sidebar.css';
+import './Styles/MainContent.css';
+
+
+const App = () => {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar style={{ flex: "0 0 250px" }} />
+      <MainContent style={{ flex: 1, padding: "20px" }} />
+    </div>
+  );
+};
 
 export default App;
